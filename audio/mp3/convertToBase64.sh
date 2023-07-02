83c6cace260a8e7aa87084ef9c04f90584eee257
garydavenport73 <software@garydavenport.com>
2023-07-02 10:16:39 -0400

    improved style of bash script for making sound libary fixed long delay at beginning of bottles game

--- audio/mp3/convertToBase64.sh
+++ audio/mp3/convertToBase64.sh
@@ -11,19 +11,18 @@ do
     #show new filename
     echo $filename;
     #start build file
-    echo "let "$baseName"=\"data:audio/mpeg;base64," >temp$filename;
-    base64 $file >>temp$filename;
-    echo "\";" >>temp$filename;
-    #remove new lines which automatically happens in base64 command
-    #then write temp file to filename
-    tr --delete '\n' < temp$filename >$filename;
-    #rm the temp file  
-    rm temp$filename; 
-    #add 2 new lines to file
-    echo >>$filename;
-    echo >>$filename;
-    #add contents of file to library
-    cat $filename >> soundLibrary.js;
-    #currently removing files and just keeping libary
-    rm $filename;
+    str="";
+    echo $str;
+    str=$str"let "$baseName" = \"data:audio/mpeg;base64,";
+    echo $str;
+    str=$str$(base64 $file|tr -d \\n);
+    #str=$str$(base64 $file);
+    str=$str\"\;;
+    #str=$str\"\n\n;
+    echo $str>$filename;
+    #add to sound library
+    echo $str>>soundLibrary.js;
+    #extra line in libary for readability
+    echo >>soundLibrary.js;
+
 done;
